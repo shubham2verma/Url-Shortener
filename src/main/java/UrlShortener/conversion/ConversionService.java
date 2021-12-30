06a7793c8ba9ba9f03666582ed53c6d591e4cbde
shubham2verma <shubhamverma7227@gmail.com>
2021-12-30 19:55:05 +0530

    Bonus-1: Modified the conversionService file to store the 	         long url and short url pair in the longAndShortURL.txt file. 	         Now, even if the application restarts, we can get the short url for 		 the long urls which are already stored in the longAndShortURL.txt file.

--- src/main/java/UrlShortener/conversion/ConversionService.java
+++ src/main/java/UrlShortener/conversion/ConversionService.java
@@ -1,16 +1,119 @@
 package UrlShortener.conversion;
 
 import java.util.HashMap;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter; 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConversionService {
 
 	HashMap<String, String> map = new HashMap<>();
-	Long number = 1000000000L;
+	String longAndShortUrlFileName = "longAndShortURL.txt";
+	String numberFileName = "number.txt";
 	String domain = "https://www.urlShortener.com/";
 	String errorMsg = "Invalid URL has been sent for the conversion";
+	Long number = 1000000000L;
+	
+	// constructor
+	public ConversionService(){
+		initialise();
+	}
+	
+	public void initialise() {
+		// Check if longAndShortUrlFileName exists, if not create an empty file
+		boolean isPresent1 = checkIfFileExists(longAndShortUrlFileName);
+		
+		if(isPresent1 == true){
+			// fill hashmap using the LongShortUrl
+			fillMap(longAndShortUrlFileName, map);
+		}
+		
+		// Check if numberFileName exists, if not create an empty file
+		boolean isPresent2 = checkIfFileExists(numberFileName);
+		if(isPresent2 == false){
+			// fill the numberFileName with 10^9
+			fillnumberFileName(numberFileName, number);
+		}
+		
+		// Get number
+		number = getNumber(numberFileName);		
+	}
+	
+	// To fill number in the numberFileName
+	public void fillnumberFileName(String fileName, Long number){
+		try {
+			FileWriter writeNumber = new FileWriter(fileName);
+			writeNumber.write("" + number); 
+			writeNumber.close();
+		}  catch(IOException exception){
+			System.out.println("An unexpected error is occurred.");  
+			exception.printStackTrace(); 
+		}
+	}
+	
+	// To read and get number from numberFileName
+	public Long getNumber(String fileName){
+		Long num = 0L;
+		try {
+			InputStream inputStream = new FileInputStream(fileName);
+	        InputStreamReader inputStreamReader = new InputStreamReader(inputStream); 
+	        BufferedReader br = new BufferedReader(inputStreamReader);	
+			String line;
+	
+			while((line = br.readLine()) != null) {
+				num = Long.parseLong(line);
+			}
+			br.close();
+		} catch(IOException exception){
+			System.out.println("An unexpected error is occurred.");  
+			exception.printStackTrace(); 
+		}
+		
+		return num;
+	}
+
+	// To check if fileName exists
+	public boolean checkIfFileExists(String fileName){
+		boolean isPresent = true;
+		try{
+			File file = new File(fileName);
+
+			if(file.createNewFile() == true){
+				isPresent = false;		
+			}
+
+		} catch(IOException exception){
+			System.out.println("An unexpected error is occurred.");  
+			exception.printStackTrace(); 
+		}
+		return isPresent;
+	}
+	
+	// To fill map with the long url and short url from longAndShortUrlFileName
+	public void fillMap(String longAndShortUrlFileName, HashMap<String, String> map){
+		try {
+			InputStream inputStream = new FileInputStream(longAndShortUrlFileName);
+	        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+	        BufferedReader br = new BufferedReader(inputStreamReader);	
+			String line;
+	
+			while((line = br.readLine()) != null) {
+				
+				String[] arrOfStr = line.split(":", 2);
+				map.put(arrOfStr[0], arrOfStr[1]);
+			}
+			br.close();
+		} catch(IOException exception){
+			System.out.println("An unexpected error is occurred.");  
+			exception.printStackTrace(); 		
+		}
+	}
 	
 	public String convert(String longUrl) {
 
@@ -34,13 +137,33 @@ public class ConversionService {
 		
 		// Store the pair (long Url : short Url) in the map
 		storeInMap(shortUrl, longUrl);
-		
+	
+		// Store Long and Short URL in file
+		storeInFile(longAndShortUrlFileName, shortUrl, longUrl);
+	
 		// Increment number for next long Url
 		number += 1;
+		
+		// Update the number in the file for next iteration
+		fillnumberFileName(numberFileName, number);
 		
 		return domain + map.get(longUrl);
 	}
 	
+	// To store long and short url in longAndShortUrlFileName
+	public void storeInFile(String fileName, String shortUrl, String longUrl){
+		
+		try {
+			FileWriter writeFile = new FileWriter(fileName, true);
+			writeFile.write(longUrl + ":" + shortUrl + "\n"); 
+			writeFile.close();    		
+		} catch(IOException exception){
+			System.out.println("An unexpected error is occurred.");  
+			exception.printStackTrace(); 
+		}
+	} 	
+
+	// To validate long url
 	public boolean validateURL(String longUrl) {
 		boolean isValid = false;
 		
@@ -52,7 +175,8 @@ public class ConversionService {
 		
 		return isValid;
 	}
-	
+
+	// To sanitize long url
 	public String sanitizeURL(String url) {
 		if (url.substring(0, 7).equals("http://"))
 			url = url.substring(7);
